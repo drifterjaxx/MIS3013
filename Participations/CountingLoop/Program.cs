f743c58fe0e35c2521b0b4eb3409ab2cd6cc2439
Joseph Cartwright <jacartwright.ok@ou.edu>
2024-02-06 17:20:56 -0600

    New program added

---
+++ Participations/CountingLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿//This program allows the user to enter a number they want to count to, and the interval at which they want to count.
+//The program will then count to the number the user entered, at the interval the user entered, and display the numbers as it counts.
+//The program will start counting at the interval the user entered.
+
+Console.WriteLine("Enter a number to count to: ");
+int countTo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter an interval to count by: ");
+int interval = Convert.ToInt32(Console.ReadLine());
+
+for (int i = interval; i <= countTo; i += interval)
+{
+    Console.WriteLine(i);
+}
---
+++ Participations/FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a program that will, for all number between 1 and 900, print 'Fizz' if the number is divisible by 3,
+//'Buzz' if the number is divisible by 5, and 'FizzBuzz' if the number is divisible by both 3 and 5.
+//If the number is not divisible by either 3 or 5, the program will print the number itself.
+
+Console.WriteLine("Enter a number between 1 and 900: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(
+    number % 3 == 0 && number % 5 == 0 ? "FizzBuzz" :
+       number % 3 == 0 ? "Fizz" :
+          number % 5 == 0 ? "Buzz" :
+             number.ToString()
+             );
