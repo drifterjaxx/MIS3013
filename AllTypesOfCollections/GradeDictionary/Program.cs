d8bbc76fdc01b89b07143c457ec5967d5df1285d
Joseph Cartwright <jacartwright.ok@ou.edu>
2024-02-29 17:42:42 -0600

    New programs added

---
+++ AllTypesOfCollections/GradeDictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿//Create a dictionary that will hold multiple values.
+//The key will be the course code (e.g. MIS3013, MIS3383, etc.) and the values will be all of the students final grades (e.g. 0.95, 0.8, etc.).
+//Add at least 3 courses codes and at least 3 grades in each one.
+//Calculate the average for each course code and output it to the user, make sure to format it as a percent.
+//If you have time, convert #3 to getting input from the user and making sure the key doesn't exist prior to adding it
+
+Dictionary <string, List<double>> gradeDictionary = new Dictionary<string, List<double>>();
+
+gradeDictionary.Add("MIS3013", new List<double>());
+gradeDictionary["MIS3013"].Add(0.9751);
+gradeDictionary["MIS3013"].Add(0.8863);
+gradeDictionary["MIS3013"].Add(0.6942);
+gradeDictionary.Add("MIS3383", new List<double>());
+gradeDictionary["MIS3383"].Add(0.9991);
+gradeDictionary["MIS3383"].Add(0.7889);
+gradeDictionary["MIS3383"].Add(0.9142);
+gradeDictionary.Add("MIS4340", new List<double>());
+gradeDictionary["MIS4340"].Add(0.6568);
+gradeDictionary["MIS4340"].Add(0.4269);
+gradeDictionary["MIS4340"].Add(0.9987);
+
+for (int i = 0; i < gradeDictionary.Count; i++)
+{
+    string courseCode = gradeDictionary.ElementAt(i).Key;
+    List<double> grades = gradeDictionary[courseCode];
+    double total = 0;
+    for (int j = 0; j < grades.Count; j++)
+    {
+        total += grades[j];
+    }
+    double average = total / grades.Count;
+    Console.WriteLine($"The average for {courseCode} is {average.ToString("P")}");
+}
---
+++ AllTypesOfCollections/GradeList/Program.cs
@@ -0,0 +1,36 @@
+﻿//Create a list that will hold all of the users grades.  Keep prompting the user if they have another grade to enter until they say 'no'.
+//Once you have all of the inputs, use a for each loop to iterate through the list and calculate the average.  Output the users grade average.
+
+List<double> grades = new List<double>();
+string input;
+double grade;
+
+while (true)
+{
+    Console.WriteLine("Do you have a grade to enter? (yes/no)");
+    input = Console.ReadLine();
+    if (input == "yes")
+    {
+        Console.WriteLine("Enter the grade: ");
+        grade = Convert.ToDouble(Console.ReadLine());
+        grades.Add(grade);
+    }
+    else if (input == "no")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+    }
+}
+
+double total = 0;
+for (int i = 0; i < grades.Count; i++)
+{
+    total = total + grades[i];
+
+}
+
+double average = total / grades.Count;
+Console.WriteLine("The average grade is " + average.ToString("0.00"));
