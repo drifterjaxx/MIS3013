25d1c09f06c1ea6194ef246d07e90b712c349ad3
Joseph Cartwright <jacartwright.ok@ou.edu>
2024-04-23 17:31:42 -0500

    New program

---
+++ EasterBunny/EasterBunny/EasterBunnyClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasterBunny
+{
+    internal class Bunny
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string DeliveryDate { get; set; }
+        public int BasketSize { get; set; }
+        public string EggColor { get; set; }
+        public int EggCount { get; set; }
+        public string Location { get; set; }
+        public string FavoriteColor { get; set; }
+
+        public Bunny()
+        {
+            Name = "";
+            Age = 0;
+            DeliveryDate = "";
+            BasketSize = 0;
+            EggColor = "";
+            EggCount = 0;
+            Location = "";
+            FavoriteColor = "";
+        }
+
+        public Bunny(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            DeliveryDate = DeliveryDate;
+            BasketSize = BasketSize;
+            EggColor = EggColor;
+            EggCount = EggCount;
+            Location = Location;
+            FavoriteColor = FavoriteColor;
+        }
+
+        //HowManyEasterBaskets method: This method will determine how many Easter baskets the Easter Bunny needs to carry all of their Eggs.
+        //Use the Easter Bunnies EggCount and BasketSize to see how many baskets they need to carry all of their eggs.  Round up to the closest whole number.
+        public int HowManyEasterBaskets()
+        {
+            return (int)Math.Ceiling((double)EggCount / BasketSize);
+        }
+
+        //ToString method: This method will return a string that has all of the values for the Easter Bunny in a properly formatted string.
+        public override string ToString()
+        {
+            return $"Bunny: {Name}, Age: {Age}, Delivery Date: {DeliveryDate}, Basket Size: {BasketSize}, Egg Color: {EggColor}, Egg Count: {EggCount}, Location: {Location}, Favorite Color: {FavoriteColor}";
+        }
+    }
+}
---
+++ EasterBunny/EasterBunny/Program.cs
@@ -0,0 +1,58 @@
+﻿//This program reads in a .csv file. It calculates the total number of eggs from all bunnies, outputs all bunnies older than 75, and all bunnies younger than 20.
+//When you output an Easter Bunny, make sure to show all of their information in a properly formatted sentence with how many Easter Baskets the Bunny needs.
+
+using EasterBunny;
+
+//Read in the .csv file to a list.
+string[] lines = File.ReadAllLines("Easter_Bunny.csv");
+List<Bunny> bunnies = new List<Bunny>();
+
+for (int i = 1; i < lines.Length; i++)
+{
+    string[] parts = lines[i].Split(',');
+    Bunny bunny = new Bunny();
+    bunny.Name = parts[0];
+    bunny.Age = int.Parse(parts[1]);
+    bunny.DeliveryDate = parts[7];
+    bunny.BasketSize = int.Parse(parts[6]);
+    bunny.EggColor = parts[5];
+    bunny.EggCount = int.Parse(parts[4]);
+    bunny.Location = parts[3];
+    bunny.FavoriteColor = parts[2];
+
+    bunnies.Add(bunny);
+}
+
+//Calculate the total number of eggs from all bunnies.
+int totalEggs = 0;
+foreach (Bunny bunny in bunnies)
+{
+    totalEggs += bunny.EggCount;
+}
+Console.WriteLine("----------------");
+Console.WriteLine($"Total Eggs: {totalEggs}");
+Console.WriteLine("----------------");
+
+//Output all bunnies older than 75.
+Console.WriteLine("----------------");
+Console.WriteLine("Bunnies older than 75");
+Console.WriteLine("----------------");
+foreach (Bunny bunny in bunnies)
+{
+    if (bunny.Age > 75)
+    {
+        Console.WriteLine($"{bunny.ToString()} needs {bunny.HowManyEasterBaskets()} Easter Baskets.");
+    }
+}
+
+//Output all bunnies younger than 20.
+Console.WriteLine("----------------");
+Console.WriteLine("Bunnies younger than 20");
+Console.WriteLine("----------------");
+foreach (Bunny bunny in bunnies)
+{
+    if (bunny.Age < 20)
+    {
+        Console.WriteLine($"{bunny.ToString()} needs {bunny.HowManyEasterBaskets()} Easter Baskets.");
+    }
+}
